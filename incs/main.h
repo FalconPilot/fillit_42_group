bd1608ba3ce8e58aed49a725831e09ba4e87a36e
Amelie LALLEMAND <alallema@e2r7p3.42.fr>
2015-12-10 11:43:51 +0100

    head dans le header changement nom de fonction

--- incs/main.h
+++ incs/main.h
@@ -6,11 +6,8 @@
 /*   By: alallema <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/09 13:12:20 by alallema          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2015/12/09 19:37:30 by alallema         ###   ########.fr       */
-=======
+/*   Updated: 2015/12/10 11:38:45 by alallema         ###   ########.fr       */
 /*   Updated: 2015/12/09 17:14:45 by rpallies         ###   ########.fr       */
->>>>>>> ea2f2479a9c5a012c82ebe1d516961a350ff98b8
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +29,7 @@ typedef struct			s_tetr
 void					ft_lst_pushback(t_tetr **begin, char **data);
 t_tetr					*ft_create_elem(char **pattern);
 t_tetr					*ft_check_tab(char *s, int n);
-void					ft_create_tab(char *av);
+void					ft_read_file(char *av);
 
 void    				error_display(int errid);
 int						check_tetr(int fd);
